a94e9a2819d6b5d53a9568f86d1e193bf3bf948b
Liz <epineda@mic.com>
2018-03-16 15:01:07 -0400

    rename, add additional try/catch block for inputPath

--- bin/cli.js
+++ bin/cli.js
@@ -12,7 +12,7 @@ function usage() {
   console.log('');
   console.log('OPTIONS:');
   console.log('  --help                   Show this message');
-  console.log('  --output                 Write AMP HTML to file');
+  console.log('  --output                 Write AMP HTML to print to standard output');
   process.exit(1);
 }
 
@@ -20,27 +20,35 @@ if (argv.help) {
   usage();
 }
 
-const input = argv._[0];
-const output = argv.output;
-if (!input) {
+const inputPath = argv._[0];
+const outputPath = argv.output;
+if (!inputPath) {
   usage();
 }
 
 let json;
+let validFile;
+
 try {
-  json = JSON.parse(fs.readFileSync(input, 'utf8'));
+  validFile = fs.readFileSync(inputPath, 'utf8');
 } catch (e) {
-  console.error(`Unable to load json ${input}`);
+  console.error(`${inputPath} does not exist`);
+  process.exit(1);
+}
+try {
+  json = JSON.parse(validFile);
+} catch (e) {
+  console.error(`Unable to load json ${validFile}`);
   process.exit(1);
 }
 
 const ampHtml = storyJsonToAmp(json);
 
-if (output) {
+if (outputPath) {
   try {
-    fs.writeFileSync(output, ampHtml);
+    fs.writeFileSync(outputPath, ampHtml);
   } catch (error) {
-    console.error(`There was an error writing file ${output}`);
+    console.error(`There was an error writing file ${outputPath}`);
   }
 } else {
   console.log(ampHtml);
